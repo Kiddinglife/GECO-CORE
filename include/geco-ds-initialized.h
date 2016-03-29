5c984da68c4e36d2af14acddc913be73dfa85a2e
Kiddinglife <jakezhang1989@hotmail.com>
2016-03-29 21:57:58 +1100

    FINISHUP GECO-DS-INITIALIZED.H

--- include/geco-ds-initialized.h
+++ include/geco-ds-initialized.h
@@ -81,6 +81,7 @@
 
 #include <string.h>
 #include "geco-ds-algo-base.h"
+#include "geco-ds-ctor.h"
 
 GECO_BEGIN_NAMESPACE
 
@@ -203,6 +204,135 @@ inline pair<InputIter, ForwardIter>
 uninitialized_copy_n(InputIter first, Size count, ForwardIter result)
 {
     return uninitialized_copy_n_aux1(first, count, result, GET_ITER_CATEGORY(first));
+}
+
+// Valid if copy construction is equivalent to assignment, and if the
+// destructor is trivial.
+template <class ForwardIter, class Type>
+inline void
+uninitialized_fill_aux2(ForwardIter start, ForwardIter end, const Type& value, true_type)
+{
+    fill(start, end, value);
+}
+
+template <class ForwardIter, class Type>
+void
+uninitialized_fill_aux2(ForwardIter start, ForwardIter end, const Type& value, false_type)
+{
+    ForwardIter cur = start;
+    GECO_TRY
+    {
+        for (; cur != end; ++cur)
+        {
+            construct(&*cur, value);
+        }
+    }GECO_UNWIND(destroy(start, cur));
+}
+
+template <class ForwardIter, class Type, class ValueType>
+inline void
+uninitialized_fill_aux1(ForwardIter start, ForwardIter end, const Type& value, ValueType*)
+{
+    typedef typename type_traitor<ValueType>::is_pod_type is_pod;
+    uninitialized_copy_aux2(start, end, value, is_pod());
+}
+
+template <class ForwardIter, class Type>
+inline void
+uninitialized_fill(ForwardIter start, ForwardIter end, const Type& value)
+{
+    uninitialized_fill_aux1(start, end, GET_VALUE_TYPE(start));
+}
+
+// Valid if copy construction is equivalent to assignment, and if the
+//  destructor is trivial.
+template <class ForwardIter, class Size, class Type>
+inline ForwardIter
+uninitialized_fill_n_aux2(ForwardIter start, Size __n,
+const Type& value, true_type)
+{
+    return fill_n(start, __n, value);
+}
+
+template <class ForwardIter, class Size, class Type>
+ForwardIter
+uninitialized_fill_n_aux2(ForwardIter start, Size __n, const Type& value, false_type)
+{
+    ForwardIter __cur = start;
+    GECO_TRY
+    {
+        for (; __n > 0; --__n, ++__cur)
+        construct(&*__cur, value);
+        return __cur;
+    }GECO_UNWIND(destroy(start, __cur));
+}
+
+template <class ForwardIter, class Size, class Type, class ValueType>
+inline ForwardIter
+uninitialized_fill_n_aux1(ForwardIter start, Size __n, const Type& value, ValueType*)
+{
+    typedef typename type_traitor<ValueType>::is_pod_type _Is_POD;
+    return uninitialized_fill_n_aux2(start, __n, value, _Is_POD());
+}
+
+template <class ForwardIter, class Size, class Type>
+inline ForwardIter
+uninitialized_fill_n(ForwardIter start, Size __n, const Type& value)
+{
+    return uninitialized_fill_n_aux1(start, __n, value, GET_VALUE_TYPE(start));
+}
+
+// Extensions:
+// uninitialized_copy_copy, uninitialized_copy_fill, 
+// uninitialized_fill_copy.
+
+// uninitialized_copy_copy
+// Copies [first1, last1) into [result, result + (last1 - first1)), and
+//  copies [first2, last2) into
+//  [result, result + (last1 - first1) + (last2 - first2)).
+template <class InputIter1, class InputIter2, class ForwardIter>
+inline ForwardIter
+uninitialized_copy_copy(InputIter1 start, InputIter1 end,
+InputIter2 start2, InputIter2 end2,
+ForwardIter dest)
+{
+    ForwardIter mid = uninitialized_copy(start, end, dest);
+    GECO_TRY
+    {
+        return uninitialized_copy(start2, end2, mid);
+    }GECO_UNWIND(destroy(dest, mid));
+}
+
+// __uninitialized_fill_copy
+// Fills [result, mid) with x, and copies [first, last) into
+//  [mid, mid + (last - first)).
+template <class ForwardIter, class Type, class InputIter>
+inline ForwardIter
+uninitialized_fill_copy(ForwardIter dest, ForwardIter mid,
+InputIter start, InputIter end,
+const Type& value)
+{
+    uninitialized_fill(dest, mid, value);
+    GECO_TRY
+    {
+        return uninitialized_copy(start, end, mid);
+    }GECO_UNWIND(destroy(dest, mid));
+}
+
+// __uninitialized_copy_fill
+// Copies [first1, last1) into [first2, first2 + (last1 - first1)), and
+//  fills [first2 + (last1 - first1), last2) with x.
+template <class InputIter, class ForwardIter, class Type>
+inline void
+uninitialized_copy_fill(InputIter start, InputIter end,
+ForwardIter start2, ForwardIter end2,
+const Type& value)
+{
+    ForwardIter mid2 = uninitialized_copy(start, end, start2);
+    GECO_TRY
+    {
+        uninitialized_fill(mid2, end2, value);
+    }GECO_UNWIND(destroy(start2, mid2));
 }
 
 GECO_END_NAMESPACE
